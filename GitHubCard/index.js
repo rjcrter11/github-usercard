8b7a3ca2feb11961327e26eb13e14af9ae666bb5
rjcrter11 <rjcrter11@gmail.com>
2020-01-16 16:28:32 -0600

    finished mvp...might have done the stretch?

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/rjcrter11")
+  .then((response) => {
+    Object.keys(response).forEach((item) => {
+      cards.append(gitCards(response[item]));
+    });
+  })
+  .catch((error) => {
+    console.log("not returned", error);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +34,24 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
+const cards = document.querySelector(".cards");
+followersArray.forEach((followed) => {
+  axios
+    .get(`https://api.github.com/users/${followed}`)
+    .then((response) => {
+      cards.append(gitCards(response.data));
+    })
+    .catch((error) => {
+      console.log("not returned", error);
+    });
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +72,43 @@ const followersArray = [];
 </div>
 
 */
+
+function gitCards(arg) {
+  const card = document.createElement("div"),
+    img = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    name = document.createElement("h3"),
+    userName = document.createElement("p"),
+    location = document.createElement("p"),
+    profile = document.createElement("p"),
+    anchor = document.createElement("a"),
+    followers = document.createElement("p"),
+    following = document.createElement("p"),
+    bio = document.createElement("p");
+
+  card.append(img);
+  card.append(cardInfo);
+  cardInfo.append(name, userName, location, profile, followers, following, bio);
+  profile.append(anchor);
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  userName.classList.add("username");
+
+  anchor.href = arg.html_url;
+  img.src = arg.avatar_url;
+  name.textContent = arg.name;
+  userName.textContent = arg.login;
+
+  location.textContent = `Location: ${arg.location}`;
+  profile.textContent = `Profile: ${arg.html_url}`;
+  followers.textContent = `Followers: ${arg.followers}`;
+  following.textContent = `Following: ${arg.following}`;
+  bio.textContent = `Bio: ${arg.bio}`;
+
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
